973adb7a699a7e76d3b838c55ab561e6adf50344
Agabi Seidaliyev <quhanWorkEmail@gmail.com>
2023-05-14 12:51:45 +0600

    init

---
+++ src/main/java/kz/fakewhatsapp/fakewhatsapp/FakewhatsappApplication.java
@@ -0,0 +1,13 @@
+package kz.fakewhatsapp.fakewhatsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakewhatsappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakewhatsappApplication.class, args);
+	}
+
+}
