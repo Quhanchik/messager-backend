4eb3343e05618e86a8ceeab9b0e2650939d5b28b
Agabi Seidaliyev <quhanWorkEmail@gmail.com>
2023-09-01 12:10:55 +0600

    update5

--- src/main/java/kz/fakewhatsapp/fakewhatsapp/config/WebSocketConfig.java
+++ src/main/java/kz/fakewhatsapp/fakewhatsapp/config/WebSocketConfig.java
@@ -22,10 +22,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry
                                                registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:3000/**").withSockJS();
+                .setAllowedOrigins("http://quhan.site/**").withSockJS();
 
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:3000/**");
+                .setAllowedOrigins("http://quhan.site/**");
     }
     @Override
     public void configureWebSocketTransport(WebSocketTransportRegistration registry)
